15b308b67524fee6bda1a6f576515233cbbcff31
Victor Christensen <victor.christensen@cox.net>
2017-07-12 01:25:31 +0000

    firehose track 5, lesson 9 validating grams

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -11,10 +11,14 @@ class GramsController < ApplicationController
   def index
   end
   
-# create method added 11 jul 17 for adding grams to db (lesson 7)
+# create method added 11 jul 17 for adding grams to db, validating grams (lessons 7, 9)
   def create
     @gram = Gram.create(gram_params)    # save gram to db
-    redirect_to root_path
+    if @gram.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
   
 # private gram_params method added 11 jul 17 for adding grams to db (lesson 7)
--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,2 +1,6 @@
 class Gram < ApplicationRecord
+  
+# next line added 11 jul 17 for gram validations (lesson 9)
+  validates :message, presence: true      # validates messages aren't blank
+  
 end
